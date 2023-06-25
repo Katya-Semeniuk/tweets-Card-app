fe78e7ac005ec6fd119828fb6a617ecb813fc9e1
Katya-Semeniuk <katyasemeniuk@gmail.com>
2023-06-25 11:30:24 +0200

    canceled viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "/tweets-Card-app/",
+  // base: "/tweets-Card-app/",
   plugins: [react()],
 });
